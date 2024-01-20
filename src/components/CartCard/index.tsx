88445a4ed21165c5dbe0987e4b9a25590d3fcf90
marcosviana91 <marcos.viana.91@gmail.com>
2024-01-20 11:27:49 -0300

    +Remove item do carrinho

--- src/components/CartCard/index.tsx
+++ src/components/CartCard/index.tsx
@@ -1,3 +1,6 @@
+import { useDispatch } from 'react-redux'
+import { removeItem } from '../../store/reducers/cart'
+
 import StyledCartCard from './style'
 
 import lixeira from '../../assets/images/lixeira_icon.png'
@@ -5,12 +8,18 @@ import { MenuItemProps } from '../../utilities/types'
 import { formataPreco } from '../../utilities/helper'
 
 function CartCard(props: MenuItemProps) {
+  const dispatch = useDispatch()
   return (
     <StyledCartCard>
       <img src={props.foto} alt={props.nome} />
       <h3>{props.nome}</h3>
       <span>{formataPreco(props.preco)}</span>
-      <button type="button">
+      <button
+        type="button"
+        onClick={() => {
+          dispatch(removeItem(props.id))
+        }}
+      >
         <img src={lixeira} />
       </button>
     </StyledCartCard>
